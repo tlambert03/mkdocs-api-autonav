d6693aa188c8f4914e8884b04159acdb8d9d20ae
Talley Lambert <talley.lambert@gmail.com>
2025-05-18 16:13:29 -0400

    fix: fix integration with awesome-nav (#17)
    * fix: fix integration with awesome-nav
    
    * rename
    
    * import or skip

--- tests/test_mkdocs_api_autonav.py
+++ tests/test_mkdocs_api_autonav.py
@@ -236,3 +236,20 @@ def test_index_py_module(repo1: Path) -> None:
     assert (lib / "index.html").is_file()
     assert (sub_mod := lib / "index_py").is_dir()
     assert (sub_mod / "index.html").is_file()
+
+
+def test_awesome_nav_compat(repo1: Path) -> None:
+    pytest.importorskip("mkdocs_awesome_nav")
+    nav = repo1.joinpath("docs", ".nav.yml")
+    nav.write_text('nav:\n  - "index.md"\n')
+
+    cfg = cfg_dict()
+    cfg["plugins"].insert(0, "awesome-nav")
+
+    mkdocs_yml = repo1 / "mkdocs.yml"
+    mkdocs_yml.write_text(yaml.safe_dump(cfg))
+    _build_command(str(mkdocs_yml))
+    assert (ref := repo1 / "site" / "reference").is_dir()
+    assert (lib := ref / "my_library").is_dir()
+    assert (lib / "index.html").is_file()
+    assert (lib / "submod").is_dir()
